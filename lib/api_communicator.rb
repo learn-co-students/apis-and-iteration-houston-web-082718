6512e197db658710e6fc3132475c427ce80896c6
Naeblis <johnmlowery@gmail.com>
2018-08-28 15:08:52 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,15 +2,30 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_movies_from_api(input_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
+  person_info = response_hash["results"].find do |character|
+    character["name"].downcase == input_name
+  end
 
+  if !person_info
+    return nil
+  end
+
+  films_array = []
+  person_info["films"].each do |film|
+    films_array << JSON.parse(RestClient.get(film))
+  end
+  films_array.each do |film|
+    film.delete_if do |film_property_key, film_property_value|
+      film_property_key != "title" && film_property_key != "episode_id" && film_property_key != "opening_crawl"
+    end
+  end
+  films_array = sort_films_by_episode_id(films_array)
+  # films_array.select do
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -22,16 +37,26 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def sort_films_by_episode_id(films_array)
+  films_array.sort do |film1, film2|
+    film1["episode_id"] <=> film2["episode_id"]
+  end
+end
+
+def print_movies(films_array)
+  films_array.each do |film|
+    puts film["title"]
+    puts "Episode: #{film["episode_id"]}"
+    puts film["opening_crawl"]
+    puts "*" * 30
+  end
 end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+  if films_array
+    print_movies(films_array)
+  else
+    puts "do you even star wars"
+  end
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
