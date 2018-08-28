4680a51c63b3b6316bf7db7086629abaf5a2dde8
Naeblis <johnmlowery@gmail.com>
2018-08-28 15:43:27 -0500

    made everything fantastic

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,37 +4,49 @@ require 'pry'
 
 def get_character_movies_from_api(input_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
-  person_info = response_hash["results"].find do |character|
-    character["name"].downcase == input_name
-  end
-
+  person_info = find_info_for_character(input_name)
   if !person_info
     return nil
   end
+  print_found_characters_name(input_name, person_info)
 
-  films_array = []
-  person_info["films"].each do |film|
-    films_array << JSON.parse(RestClient.get(film))
+  films_array = get_films_for_character(person_info)
+
+  trimmed_film_info = trim_film_info(films_array)
+
+  sort_films_by_episode_id(trimmed_film_info)
+end
+
+def find_info_for_character(input_name)
+  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_hash = JSON.parse(response_string)
+
+  response_hash["results"].find do |character|
+    character.values.include?(input_name)
   end
+end
+
+def trim_film_info(films_array)
   films_array.each do |film|
     film.delete_if do |film_property_key, film_property_value|
       film_property_key != "title" && film_property_key != "episode_id" && film_property_key != "opening_crawl"
     end
   end
-  films_array = sort_films_by_episode_id(films_array)
-  # films_array.select do
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  films_array
+end
+
+def get_films_for_character(person_info)
+  films_array = []
+  person_info["films"].each do |film|
+    films_array << JSON.parse(RestClient.get(film))
+  end
+  films_array
+end
+
+def print_found_characters_name(input_name, person_info)
+  if person_info["name"] != input_name
+    puts person_info["name"]
+  end
 end
 
 def sort_films_by_episode_id(films_array)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -5,6 +5,6 @@ end
 
 def get_character_from_user
   puts "please enter a character"
-   gets.chomp.downcase
+  gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
 end
